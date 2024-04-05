d7bcb55f11a144ac723f0eb268f87785fdc374c1
Muhammad Saqlain <107932964+saqlain017@users.noreply.github.com>
2024-04-06 03:50:52 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: 'https://healthcare-zeta-eight.vercel.app/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
