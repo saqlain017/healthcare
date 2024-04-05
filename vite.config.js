11b0482bd00aa734fabd0dc3c1a0a720f178c98f
Muhammad Saqlain <107932964+saqlain017@users.noreply.github.com>
2024-04-06 02:12:57 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: 'https://healthcare-rho-gules.vercel.app/',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
